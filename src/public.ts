0bc550ecdec0db2ee6c8c674c93b14d5d6db3a35
abztabz <tsngdn72fc@privaterelay.appleid.com>
2026-08-15 12:44:03 +0400

    Define complete standalone public API

---
+++ src/public.ts
@@ -0,0 +1,6 @@
+export * from "./capabilities.js";
+export * from "./gateway.js";
+export * from "./http.js";
+export * from "./registry.js";
+export * from "./registry-view.js";
+export * from "./providers/all.js";
